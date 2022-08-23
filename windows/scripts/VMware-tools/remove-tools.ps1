0aad659cddc5a6e71f54bc9375036efc5db7c3ee
Omar Reda <omarreda5050@gmail.com>
2022-08-23 15:04:47 +0200

    Script Format

--- windows/scripts/VMware-tools/remove-tools.ps1
+++ windows/scripts/VMware-tools/remove-tools.ps1
@@ -1,34 +1,32 @@
 
-Write-Host "`n`nRemoving VMware tools from your VM machine`n`n"   -ForegroundColor red -BackgroundColor white
+Write-Host "`n`nRemoving VMware tools from your VM machine`n`n" -ForegroundColor red -BackgroundColor white
 
 function removevmtools {
 
-try {
-$product = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "VMware Tools"}
-$AppGUID = $product.properties["IdentifyingNumber"].value.toString()
-if ( $AppGUID.count -eq 0 )
-{
+  try {
+    
+    $product = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "VMware Tools"}
+    $AppGUID = $product.properties["IdentifyingNumber"].value.toString()
+    
+    if ( $AppGUID.count -eq 0 ) {
 
-  Write-Host  "`n`nVMware Tools not found`n`n"   -ForegroundColor red -BackgroundColor white
-  Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n"   -ForegroundColor red -BackgroundColor white
+      Write-Host  "`n`nVMware Tools not found`n`n" -ForegroundColor red -BackgroundColor white
+      Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n" -ForegroundColor red -BackgroundColor white
 
-}
+    } else {
 
-else {
+    MsiExec.exe /norestart /q/x $AppGUID REMOVE=ALL
+    Start-Sleep -s 15
+    Write-Host  "`n`nVMware Tools Has Been Uninstalled`n`n" -ForegroundColor red -BackgroundColor white
+    Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n" -ForegroundColor red -BackgroundColor white
 
-MsiExec.exe /norestart /q/x $AppGUID REMOVE=ALL
-Start-Sleep -s 15
-Write-Host  "`n`nVMware Tools Has Been Uninstalled`n`n"   -ForegroundColor red -BackgroundColor white
-Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n"   -ForegroundColor red -BackgroundColor white
-}
+    }
+  } catch {
 
-
-}
-catch{
- Write-Host  "`n`nVMware Tools not found`n`n"   -ForegroundColor red -BackgroundColor white
- Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n"   -ForegroundColor red -BackgroundColor white
-  
-}
+  Write-Host  "`n`nVMware Tools not found`n`n" -ForegroundColor red -BackgroundColor white
+  Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n" -ForegroundColor red -BackgroundColor white
+    
+  }
 }
 
 removevmtools
