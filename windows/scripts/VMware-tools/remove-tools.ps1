6b92cd698087e0ab5ac78b4b5443e7e28b5f895b
Omar Reda <omarreda5050@gmail.com>
2022-08-23 16:20:18 +0200

    remove catch and sleep command

--- windows/scripts/VMware-tools/remove-tools.ps1
+++ windows/scripts/VMware-tools/remove-tools.ps1
@@ -15,17 +15,13 @@ function removevmtools {
 
     } else {
 
-    MsiExec.exe /norestart /q/x $AppGUID REMOVE=ALL
-    Start-Sleep -s 15
+    MsiExec.exe /norestart /q /x $AppGUID REMOVE=ALL
     Write-Host  "`n`nVMware Tools Has Been Uninstalled`n`n" -ForegroundColor red -BackgroundColor white
     Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n" -ForegroundColor red -BackgroundColor white
 
     }
-  } catch {
+  } catch { 
 
-  Write-Host  "`n`nVMware Tools not found`n`n" -ForegroundColor red -BackgroundColor white
-  Write-Host "`n`nPlease Shtudown The VM and Export it as OVA`n`n" -ForegroundColor red -BackgroundColor white
-    
   }
 }
 
