f3ccb188d549078a689141cc4f55eb9e7168550e
Omar Reda <omarreda5050@gmail.com>
2022-08-23 23:27:36 +0200

    fixed grammatical mistakes

--- windows/scripts/VMware-tools/remove-tools.ps1
+++ windows/scripts/VMware-tools/remove-tools.ps1
@@ -1,19 +1,19 @@
-Write-Host "`n`nRemoving VMware tools from your VM machine`n`n" -ForegroundColor red -BackgroundColor white
+Write-Host "`n`nRemoving VMware Tools from your VM`n`n" -ForegroundColor red -BackgroundColor white
 
 function removevmtools {
-  
+
   $product = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "VMware Tools"}
   $AppGUID = $product.properties["IdentifyingNumber"].value.toString()
   
   if ( $AppGUID.count -eq 0 ) {
 
-    Write-Host  "`n`nVMware Tools not found`n`n" -ForegroundColor red -BackgroundColor white
+    Write-Host  "`n`nVMware Tools are not found`n`n" -ForegroundColor red -BackgroundColor white
     Write-Host "`n`nPlease shutdown The VM and export it as OVA`n`n" -ForegroundColor red -BackgroundColor white
 
   } else {
 
     MsiExec.exe /norestart /q /x $AppGUID REMOVE=ALL
-    Write-Host  "`n`nVMware Tools Has Been Uninstalled`n`n" -ForegroundColor red -BackgroundColor white
+    Write-Host  "`n`nVMware Tools have been uninstalled`n`n" -ForegroundColor red -BackgroundColor white
     Write-Host "`n`nPlease shutdown The VM and export it as OVA`n`n" -ForegroundColor red -BackgroundColor white
 
   }
